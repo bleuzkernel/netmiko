d6f8b30a5b9d6f25f8962291ef611b63d703a96e
bleuzkernel <bleuzkernel@gmail.com>
2023-01-04 19:48:37 -0600

    Update ssh_autodetect.py
    Added ZTE ZXCTN platforms to SSH autodetect

--- netmiko/ssh_autodetect.py
+++ netmiko/ssh_autodetect.py
@@ -288,6 +288,12 @@ SSH_MAPPER_DICT = {
         "priority": 99,
         "dispatch": "_autodetect_std",
     },
+    "zte_zxros": {
+        "cmd": "show version",
+        "search_patterns": [r"ZTE ZXCTN Software"],
+        "priority": 99,
+        "dispatch": "_autodetect_std",
+    },
 }
 
 # Sort SSH_MAPPER_DICT such that the most common commands are first
